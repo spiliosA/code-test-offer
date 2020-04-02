8d1e8deb1c6679f89bbfdf4e83a6db24226cecca
spiliosA <spiliosalexopoulos@gmail.com>
2020-04-02 15:31:40 +0100

    initial commit

---
+++ src/main/java/com/example/codetestoffer/CodeTestOfferApplication.java
@@ -0,0 +1,13 @@
+package com.example.codetestoffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeTestOfferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeTestOfferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codetestoffer/CodeTestOfferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codetestoffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeTestOfferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
